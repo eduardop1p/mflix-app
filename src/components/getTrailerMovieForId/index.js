24862c5d47190bebdab02f64b34c138432aec2b5
Eduardo Oliveira <el702985@gmail.com>
2023-01-25 14:45:54 -0300

    liberando future mobile com trailer #3

--- src/components/getTrailerMovieForId/index.js
+++ src/components/getTrailerMovieForId/index.js
@@ -6,7 +6,7 @@ import video from '../../assets/videos/Downtown but at Night.mp4';
 
 /* eslint-disable*/
 export default function GetTrailerMovie(props) {
-  const { id } = props;
+  const { id, loadingDetails } = props;
 
   const [trailer, setTrailer] = useState([]);
 
@@ -27,11 +27,20 @@ export default function GetTrailerMovie(props) {
     getTrailer();
   }, []);
 
+  return (
+    <>
+      <div className="msg-video-trailer-error">
+        Ainda não existe um trailer pra este filme.
+      </div>
+      <video controls src={video}></video>
+    </>
+  );
+
   return trailer.length ? (
     <iframe
       width="100%"
       height="100%"
-      loading="lazy"
+      loading={loadingDetails ? loadingDetails : 'lazy'}
       src={` https://www.youtube-nocookie.com/embed/${trailer[0].key}`}
       title="YouTube video player"
       allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
